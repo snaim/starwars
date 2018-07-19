120db27f14c314535e9c7a0d3f6f69dca6d1792f
Sylvia Naim <sylvia.naim@ekino.com>
2018-07-19 14:37:57 +0200

    ui - add track thumbnail with Glide

--- app/src/main/java/com/naim/mymusic/ui/musiclist/ItemMusicModel.kt
+++ app/src/main/java/com/naim/mymusic/ui/musiclist/ItemMusicModel.kt
@@ -2,8 +2,10 @@ package com.naim.mymusic.ui.musiclist
 
 import android.support.v7.widget.CardView
 import com.airbnb.epoxy.EpoxyModel
+import com.bumptech.glide.Glide
 import com.naim.mymusic.R
 import com.naim.mymusic.ui.model.MusicModel
+import kotlinx.android.synthetic.main.ui_music_item.view.music_thumbnail
 import kotlinx.android.synthetic.main.ui_music_item.view.title
 
 /**
@@ -20,5 +22,9 @@ class ItemMusicModel(private val music: MusicModel) : EpoxyModel<CardView>() {
 
     override fun bind(view: CardView) {
         view.title.text = music.title
+
+        Glide.with(view.context)
+                .load(music.thumbnailUrl)
+                .into(view.music_thumbnail)
     }
 }
